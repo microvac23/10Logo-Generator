0346e86d89974f8b35a915d1be884de395a2f622
microvac23 <Ithanmachuca@gmail.com>
2023-08-22 22:15:31 -0500

    background color fixed, example pic added

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const inquirer = require('inquirer')
 const {Square, Circle, Triangle} = require('./lib/shapes.js')
 const fs = require('fs')
 
-function generateSVG(response) {
-    if (response.shape === 'square') {
-        var content = new Square(response.backgroundColor)
-    } else if (response.shape === 'circle') {
-        var content = new Circle(response.backgroundColor)
-    } else if (response.shape === 'triangle') {
-        var content = new Triangle(response.backgroundColor)
+// This function generates the content for the SVG file and inserts the shape content depending on the response from inquirer
+function generateSVG({shape, backgroundColor, textColor, text}) {
+    if (shape === 'square') {
+        var content = new Square(backgroundColor)
+        console.log(backgroundColor);
+    } else if (shape === 'circle') {
+        var content = new Circle(backgroundColor)
+    } else if (shape === 'triangle') {
+        var content = new Triangle(backgroundColor)
     }
 
     const image = `<svg version="1.1" height="200" width = "300" xmlns="http://www.w3.org/2000/svg">
     ${content.render()}
-    <text x="150" y="125" font-size="40" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
+    <text x="150" y="125" font-size="40" text-anchor="middle" fill="${textColor}">${text}</text>
   </svg>`
 
   return image
@@ -40,11 +42,11 @@ inquirer.prompt([
     {
         type: 'input',
         message: `What background color would you like? (Input color name or hex code "#000000")`,
-        name: `background`
+        name: `backgroundColor`
     }
 ])
 .then(response => {
-    console.log('hello', response.shape);
+    console.log(`Hello, your logo has been made and added to the examples folder!`);
     
     fs.writeFile("./examples/logo.svg", generateSVG(response), (err) => {
         if (err)
