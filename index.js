e448230a035197552710cc1564f225438be29d31
microvac23 <Ithanmachuca@gmail.com>
2023-08-22 21:51:46 -0500

    generates file, but doesn't pass shape class

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const inquirer = require('inquirer')
 const {Square, Circle, Triangle} = require('./lib/shapes.js')
+const fs = require('fs')
+
+function generateSVG(response) {
+    if (response.shape === 'square') {
+        var content = new Square(response.backgroundColor)
+    } else if (response.shape === 'circle') {
+        var content = new Circle(response.backgroundColor)
+    } else if (response.shape === 'triangle') {
+        var content = new Triangle(response.backgroundColor)
+    }
+
+    const image = `<svg version="1.1" height="200" width = "300" xmlns="http://www.w3.org/2000/svg">
+    ${content.render()}
+    <text x="150" y="125" font-size="40" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
+  </svg>`
+
+  return image
+}
+
 
 inquirer.prompt([
     {
@@ -25,4 +44,10 @@ inquirer.prompt([
     }
 ])
 .then(response => {
-    console.log('hello', response);})
+    console.log('hello', response.shape);
+    
+    fs.writeFile("./examples/logo.svg", generateSVG(response), (err) => {
+        if (err)
+          console.log(err);
+      });
+})
